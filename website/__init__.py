7ae16f2e802f2328d19ffa17c5dbdfb82a99cbba
Ralf Lopez <ralflopez3@gmail.com>
2021-07-01 13:25:54 +0800

    ::lipstick:style homepage page

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.helpers import url_for
 from flask_session import Session
 
 def create_app():
@@ -8,10 +9,13 @@ def create_app():
     # configure session
     app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     # register routes
     from .views import views
+    from .api import api
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(api, url_prefix="/api")
 
     return app
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_session import Session
+from werkzeug.utils import redirect
 
 views = Blueprint('views', __name__)
 
@@ -8,3 +9,16 @@ def index():
     # from .packages.youtube_data_api import getVideoTranscript
 
     return render_template('index.html')
+
+@views.route('/book')
+def book_invalid():
+    return redirect('/')
+
+@views.route('/book/<v_id>')
+def book(v_id):
+    book_details = {
+        'title': 'The Art of War',
+        'author': 'Sun Tzu'
+    }
+
+    return render_template('book.html', book_details=book_details, v_id=v_id)
