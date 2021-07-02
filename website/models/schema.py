049a9fdc8dd31a8c9bc3f64907a388c7a2a76033
Ralf Lopez <ralflopez3@gmail.com>
2021-07-02 14:18:30 +0800

    :card_file_box: use v_id as books primary key

--- website/models/schema.py
+++ website/models/schema.py
@@ -5,4 +5,36 @@ user_schema = """
         hash TEXT NOT NULL, 
         PRIMARY KEY(id));
     CREATE UNIQUE INDEX username ON users (username);
-    """
+"""
+
+author_schema = """
+    CREATE TABLE authors (
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY(id)
+    );
+"""
+
+book_schema = """
+    CREATE TABLE books (
+        id TEXT UNIQUE NOT NULL,
+        title TEXT,
+        author_id INTEGER,
+        contributor_id INTEGER,
+        PRIMARY KEY(id),
+        FOREIGN KEY (author_id) REFERENCES authors(id),
+        FOREIGN KEY (contributor_id) REFERENCES users(id)
+    );
+"""
+
+saves_scema = """
+    CREATE TABLE saves (
+        id INTEGER,
+        user_id INTEGER,
+        book_id INTEGER,
+        PRIMARY KEY(id),
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        FOREIGN KEY(book_id) REFERENCES books(id),
+        UNIQUE(user_id, book_id)
+    );
+"""
